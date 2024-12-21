0d2439d2be097f1bc9b010106a11531cb6d7c9b3
Ilham8600 <ilhamyunanto53@gmail.com>
2024-12-21 10:42:46 +0700

    Add files via upload

---
+++ M5_5230411277_Muhammad Ilham Yunanto.py
@@ -0,0 +1,152 @@
+class Music:
+    def __init__(self, judul, penyanyi, genre):
+        self.judul = judul  
+        self.penyanyi = penyanyi
+        self.genre = genre
+
+    def tampil(self):
+        print(f"Judul: {self.judul}, Penyanyi: {self.penyanyi}, Genre: {self.genre}")
+
+class English(Music):
+    def __init__(self, judul, penyanyi, genre):
+        super().__init__(judul, penyanyi, genre)
+
+class Indonesia(Music):
+    def __init__(self, judul, penyanyi, genre):
+        super().__init__(judul, penyanyi, genre)
+
+class Japanese(Music):
+    def __init__(self, judul, penyanyi, genre):
+        super().__init__(judul, penyanyi, genre)
+
+def add_Esong(Esongs):
+    try:
+        judul = input("Masukan Judul Lagu: ")
+        if any(Esong.judul == judul for Esong in Esongs):
+            print("Judul lagu sudah ada, lagu gagal ditambahkan.")
+            return
+        
+        penyanyi = input("Masukan Nama Penyanyi: ")
+        genre = input("Masukan Genre Musik: ")
+        new_esong = English(judul, penyanyi, genre)
+        Esongs.append(new_esong)
+        print("Lagu berhasil ditambahkan.")
+    except ValueError:
+        print("Input tidak Valid")
+
+def add_Isong(Isongs):
+    try:
+        judul = input("Masukan Judul Lagu: ")
+        if any(Isong.judul == judul for Isong in Isongs):
+            print("Judul lagu sudah ada, lagu gagal ditambahkan.")
+            return
+        
+        penyanyi = input("Masukan Nama Penyanyi: ")
+        genre = input("Masukan Genre Musik: ")
+        new_isong = Indonesia(judul, penyanyi, genre)
+        Isongs.append(new_isong)
+        print("Lagu berhasil ditambahkan.")
+    except ValueError:
+        print("Input tidak Valid")
+
+def add_Jsong(Jsongs):
+    try:
+        judul = input("Masukan Judul Lagu: ")
+        if any(Jsong.judul == judul for Jsong in Jsongs):
+            print("Judul lagu sudah ada, lagu gagal ditambahkan.")
+            return
+        
+        penyanyi = input("Masukan Nama Penyanyi: ")
+        genre = input("Masukan Genre Musik: ")
+        new_jsong = Japanese(judul, penyanyi, genre)
+        Jsongs.append(new_jsong)
+        print("Lagu berhasil ditambahkan.")
+    except ValueError:
+        print("Input tidak Valid")
+
+def Display_ESong(Esongs):
+    if not Esongs:
+        print("Tidak Ada Lagu.")
+    else:
+        for esong in Esongs:
+            esong.tampil()
+
+def Display_ISong(Isongs):
+    if not Isongs:
+        print("Tidak Ada Lagu.")
+    else:
+        for isong in Isongs:
+            isong.tampil()
+
+def Display_JSong(Jsongs):
+    if not Jsongs:
+        print("Tidak Ada Lagu.")
+    else:
+        for jsong in Jsongs:
+            jsong.tampil()
+
+
+def main():
+    Esongs = []
+    Isongs = []
+    Jsongs = []
+
+    while True:
+        print("=====Playlist Music=====")
+        print("1. English Song")
+        print("2. Indonesian Song")
+        print("3. Japanese Song")
+        print("4. Display All")
+        print("5. Search Music")
+        print("0. Exit Program")
+        pilih = input("Pilih Menu di Atas: ")
+
+        if pilih == "1":
+            while True:
+                print("=====English Song=====")
+                print("1. Display Song")
+                print("2. Add Song")
+                print("0. Kembali ke Menu Utama")
+                pilih = input("Pilih Menu di Atas: ")
+                if pilih == "1":
+                    Display_ESong(Esongs)
+                elif pilih == "2":
+                    add_Esong(Esongs)
+                elif pilih == "0":
+                    break
+
+        elif pilih == "2":
+            while True:
+                print("=====Indonesian Song=====")
+                print("1. Display Song")
+                print("2. Add Song")
+                print("0. Kembali ke Menu Utama")
+                pilih = input("Pilih Menu di Atas: ")
+                if pilih == "1":
+                    Display_ESong(Isongs)  
+                elif pilih == "2":
+                    add_Isong(Isongs)
+                elif pilih == "0":
+                    break
+
+        elif pilih == "3":
+            while True:
+                print("=====Japanese Song=====")
+                print("1. Display Song")
+                print("2. Add Song")
+                print("0. Kembali ke Menu Utama")
+                pilih = input("Pilih Menu di Atas: ")
+                if pilih == "1":
+                    Display_ESong(Jsongs)  
+                elif pilih == "2":
+                    add_Jsong(Jsongs)
+                elif pilih == "0":
+                    break
+
+        elif pilih == "0":
+            print("Anda Keluar Program")
+            break
+        else:
+            print("Pilihan Tidak Valid")
+
+main()
